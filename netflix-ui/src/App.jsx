e97a812dad1ff5ab08e4e2a30eb5861db52d200e
Siddhesh Surve <sysurve24@gmail.com>
2023-04-27 18:27:09 +0000

    Base : pages-> login singup home route

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Netflix from './pages/Netflix'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/" element={<Netflix />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
