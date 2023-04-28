b058962d96121bd99f5315deb27e662e2056122f
Siddhesh Surve <sysurve24@gmail.com>
2023-04-28 02:35:52 +0000

    Structured Singup component

---
+++ netflix-ui/src/components/BackgroundImage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BackgroundImage() {
+  return (
+    <div>BackgroundImage</div>
+  )
+}
--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import styled from "styled-components"
+import BackgroundImage from '../components/BackgroundImage';
+import Header from '../components/Header';
+
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <Container>
+      <BackgroundImage />
+      <Header />
+      <div className="body flex column a-center j-center">
+        <div className="text flex column">
+          <h1>Unlimited movies, TV shows and more</h1>
+          <h4>Watch anywhere,. Cancel Anytime</h4>
+          <h6>
+            Ready to watch? Enter your emailto create or restart membership
+          </h6>
+        </div>
+        <div className="form">
+          <input type="email" placeholder='Email Address' name='email' />
+          <input type="password" placeholder='Password' name='password' />
+          <button>Get Started</button>
+        </div>
+        <button>Login</button>
+      </div>
+    </Container>
   )
 }
+
+const Container = styled.div``;
