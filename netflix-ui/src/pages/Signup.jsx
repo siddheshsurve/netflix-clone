3fdfdd4c95d99a9e970ac3b2ea6e76898eaa898e
Siddhesh Surve <sysurve24@gmail.com>
2023-04-28 03:30:29 +0000

    styled signup -> body

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -41,5 +41,51 @@ const Container = styled.div`
     width: 100vw;
     display: grid;
     grid-template-rows: 15vh 85vh;
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        .h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        display: grid;
+        /* grid-template-columns: ; */
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          
+          font-weight: bolder;
+          font-size: 1.05rem;
+        }
+      }
+      button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          border-radius: 0.2rem;
+          font-weight: bolder;
+          font-size: 1.05rem;
+      }
+    }
   }
 `;
