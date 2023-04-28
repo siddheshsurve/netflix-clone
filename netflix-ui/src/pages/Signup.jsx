c7a0c63fbd72723a0d7ba471661c0e739d7763ce
Siddhesh Surve <sysurve24@gmail.com>
2023-04-28 02:52:35 +0000

    Structured BackgroundImage comp+placed over signup

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -8,24 +8,38 @@ export default function Signup() {
   return (
     <Container>
       <BackgroundImage />
-      <Header />
-      <div className="body flex column a-center j-center">
-        <div className="text flex column">
-          <h1>Unlimited movies, TV shows and more</h1>
-          <h4>Watch anywhere,. Cancel Anytime</h4>
-          <h6>
-            Ready to watch? Enter your emailto create or restart membership
-          </h6>
+      <div className="content">
+        <Header />
+        <div className="body flex column a-center j-center">
+          <div className="text flex column">
+            <h1>Unlimited movies, TV shows and more</h1>
+            <h4>Watch anywhere,. Cancel Anytime</h4>
+            <h6>
+              Ready to watch? Enter your emailto create or restart membership
+            </h6>
+          </div>
+          <div className="form">
+            <input type="email" placeholder='Email Address' name='email' />
+            <input type="password" placeholder='Password' name='password' />
+            <button>Get Started</button>
+          </div>
+          <button>Login</button>
         </div>
-        <div className="form">
-          <input type="email" placeholder='Email Address' name='email' />
-          <input type="password" placeholder='Password' name='password' />
-          <button>Get Started</button>
-        </div>
-        <button>Login</button>
       </div>
     </Container>
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vw;
+  }
+`;
