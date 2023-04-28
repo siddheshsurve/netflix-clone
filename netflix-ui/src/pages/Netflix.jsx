f18de055683e7c9d5ed949f72c656539a4f8700b
Siddhesh Surve <sysurve24@gmail.com>
2023-04-28 14:06:26 +0000

    created Navbar compo+Netflix->props to Navbar

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Netflix() {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false:true);
+    return () => (window.onscroll = null);
+  }
+
   return (
-    <div>Netflix</div>
+    <div>
+      <Navbar isScrolled={isScrolled} />
+    </div>
   )
 }
