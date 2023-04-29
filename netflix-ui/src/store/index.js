8175904aeb08fcb10da34f7f47953b6f74e201b8
Siddhesh Surve <sysurve24@gmail.com>
2023-04-29 04:18:42 +0000

    TMDB api setup + Redux toolkit setup

---
+++ netflix-ui/src/store/index.js
@@ -0,0 +1,20 @@
+import { configureStore, createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    movies : [],
+    genresLoaded : false,
+    genres : [],
+};
+
+const NetflixSlice = createSlice({
+    name : "Netflix",
+    initialState,
+    extraReducers:(builder) => {},
+
+});
+
+export const store = configureStore({
+    reducer : {
+        netflix : NetflixSlice.reducer,
+    },
+});
---
+++ netflix-ui/src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "50b7429378d9b7c8707b610cf18eaeea"
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3"
